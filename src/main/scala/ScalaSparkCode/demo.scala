3f2ec6b0cfe12baba3e66c7f066fec1e53769e04
nehajha <njhasaf@gmail.com>
2021-07-06 13:42:14 +0200

    adding a specific class

--- src/main/scala/ScalaSparkCode/demo.scala
+++ src/main/scala/ScalaSparkCode/demo.scala
@@ -1,7 +1,7 @@
 package ScalaSparkCode
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions{col}
 
 object demo extends App {
 
